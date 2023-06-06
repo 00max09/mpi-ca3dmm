ebe305ba85898788cd05c38f221b735283ea8ecd
Maksymilian Grochowski <mg417883@students.mimuw.edu.pl>
2023-06-06 19:16:31 +0200

    some indexes added

--- ca3dmm.cpp
+++ ca3dmm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <mpi.h>
 
 struct Proc_grid
@@ -43,26 +44,43 @@ Proc_grid get_grid_size(int num_processes, int n, int m, int k)
     return grid;
 }
 
+class ParMatrix {
+    ParMatrix()
+}
+
+
 void run_cannon(int group, int n, int m, int k, Proc_grid g, MPI_Comm* group_comm){
     int myGroupRank, myCannonGroupRank;
     MPI_Comm_rank(group_comm, &myGroupRank);
     MPI_Comm cannon_group_comm{};
-    int cannonGroup =  myGroupRank / (std::max(g.p_m, g.p_n) / std::min(g.p_m, g.p_n));
+    int cannonGroupCount = (std::max(g.p_m, g.p_n) / std::min(g.p_m, g.p_n));
+    int cannonGroupSize = g.p_m * g.p_n / cannonGroupCount; //square of some number
+    int cannonGroupAxisLength = (int) sqrt(cannonGroupSize);
+    int cannonGroup =  myGroupRank / cannonGroupCount;
     MPI_Comm_split(group_comm, cannonGroup , myGroupRank, &cannon_group_comm);
-    MPI_Comm_rank(group_comm, &cannon_group_comm);
+    MPI_Comm_rank(group_comm, &maCannonGroupRank);
     if(g.p_m <= g.p_n){ // we are not replicating A
         int A_group_x_start = ((k+g.p_k-1)/g.p_k)*group;
         int A_group_x_end = std::min(((k+g.p_k-1)/g.p_k)*(group+1), k);
+        int A_group_x_length = A_group_x_end - A_group_x_start;
         int A_group_y_start = ((m+g.p_m-1)/g.p_m)*cannonGroup;
         int A_group_y_end = std::min(((m+g.p_m-1)/g.p_m)*(cannonGroup+1), m);
-
+        int A_group_y_length = A_group_y_end - A_group_y_start;
         
         int B_group_y_start = ((k+g.p_k-1)/g.p_k)*group;
         int B_group_y_end = std::min(((k+g.p_k-1)/g.p_k)*(group+1), k);
         int B_group_x_start = 0;
         int B_group_x_end = n;
         
+        int A_me_in_group_x = myCannonGroupRank / cannonGroupAxisLength;
+        int A_me_in_group_y = myCannonGroupRank % cannonGroupAxisLength;
+        
+        int A_my_x_start = A_group_x_start + A_group_x_length / cannonGroupAxisLength * A_me_in_group_x;
+        int A_my_x_end = A_group_x_start + A_group_x_length / cannonGroupAxisLength * (A_me_in_group_x + 1);
+        int A_my_y_start = A_group_y_start + A_group_y_length / cannonGroupAxisLength * A_me_in_group_y;
+        int A_my_y_end = A_group_y_start + A_group_y_length / cannonGroupAxisLength * (A_me_in_group_y + 1);
 
+         
 
     }
     
@@ -86,7 +104,7 @@ int main(int argc, char *argv[])
     MPI_Comm group_comm{};
     MPI_Comm_split(MPI_COMM_WORLD, myRank/grid.p_k, myRank, &group_comm);
     int my_group = myRank/grid.p_k;
-    run_cannon(my_group, n, m, k, grid, &group_comm);
+    run_cannon(my_group, n, m, k, groid, &group_comm);
     MPI_Finalize();
 
 }
